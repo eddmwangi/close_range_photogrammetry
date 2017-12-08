b9b22621fb9d12192da075ea28120b97f1a69920
jodom <jodomouma@gmail.com>
2017-12-08 21:53:42 +0300

    modify L generator

--- getL.m
+++ getL.m
@@ -4,7 +4,7 @@ function L = getL(IP,x,y,X,Y,Z,R,f)
 	L = zeros(n*2,1);
 	for m=1:n
 		a=m*2; % index navigations
-		L(a-1) = -( x(m)*( R(3,1)*(X(m)-IP(1)) + R(3,2)*(Z(m)-IP(3)) - R(3,3)*(Y(m)-IP(2)) ) + f * ( R(1,1)*(X(m)-IP(1)) + R(1,2)*(Z(m)-IP(3)) - R(1,3)*(Y(m)-IP(2)) ) );
-		L(a) = -( y(m)*( R(3,1)*(X(m)-IP(1)) + R(3,2)*(Z(m)-IP(3)) - R(3,3)*(Y(m)-IP(2)) ) + f * ( R(2,1)*(X(m)-IP(1)) + R(2,2)*(Z(m)-IP(3)) - R(2,3)*(Y(m)-IP(2)) ) );
+		L(a-1) = x(m)*( R(3,1)*(X(m)-IP(1)) + R(3,2)*(Z(m)-IP(3)) - R(3,3)*(Y(m)-IP(2)) ) + f * ( R(1,1)*(X(m)-IP(1)) + R(1,2)*(Z(m)-IP(3)) - R(1,3)*(Y(m)-IP(2)) );
+		L(a) = y(m)*( R(3,1)*(X(m)-IP(1)) + R(3,2)*(Z(m)-IP(3)) - R(3,3)*(Y(m)-IP(2)) ) + f * ( R(2,1)*(X(m)-IP(1)) + R(2,2)*(Z(m)-IP(3)) - R(2,3)*(Y(m)-IP(2)) );
 	end
 end
